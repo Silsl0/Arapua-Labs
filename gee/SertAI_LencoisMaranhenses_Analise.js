a41e042025e58485f5688412633c4a9291b3ea3c
Silsl0 <s177167@dac.unicamp.br>
2025-10-04 20:37:50 -0300

    feat(gee): add main GEE pipeline

---
+++ gee/SertAI_LencoisMaranhenses_Analise.js
@@ -0,0 +1,7 @@
+mkdir -p gee
+nano gee/SertAI_LencoisMaranhenses_Analise.js   # cole o script, salve (Ctrl+O, Enter), saia (Ctrl+X)
+
+git add gee/SertAI_LencoisMaranhenses_Analise.js
+git commit -m "feat(gee): add main GEE pipeline"
+git push origin HEAD:refs/heads/main
+
